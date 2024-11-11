0f5ee11bdf8e1822a1d6f6a60b5b1b976941ea30
Akshay <akshaymangutkar951@gmail.com>
2024-11-11 17:24:21 +0530

    netflix gpt in process

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="font-bold  text-4xl text-red-600">Hello netflix</h1>
+    </div>
+  );
+}
+
+export default App;
